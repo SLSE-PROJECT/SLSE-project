91cbebdcd8c8bee9688f185256f8cdf6589b4ed1
soolyn <soolyn4867@gmail.com>
2025-04-11 17:13:16 +0900

    SLSE-project add home

---
+++ src/main/java/org/codenova/slseproject/SlseProjectApplication.java
@@ -0,0 +1,13 @@
+package org.codenova.slseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlseProjectApplication.class, args);
+    }
+
+}
