b524b508f3236a6031ee1fbd6213af76f032db96
dap <ozalice011202@gmail.com>
2025-04-14 15:47:09 +0900

    fix: renaming

--- src/main/java/org/codenova/slseproject/controller/Champion.java
+++ src/main/java/org/codenova/slseproject/controller/Champion.java
@@ -1,7 +1,6 @@
 package org.codenova.slseproject.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.codenova.slseproject.entity.Champion;
 import org.codenova.slseproject.service.ChampionAPIService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,20 +12,20 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/champion")
-public class ChampionController {
+public class Champion {
 
     private ChampionAPIService championAPIService;
 
-    public ChampionController(ChampionAPIService championAPIService) {
+    public Champion(ChampionAPIService championAPIService) {
         this.championAPIService = championAPIService;
     }
 
     @GetMapping("/list")
     public String list(Model model) throws JsonProcessingException {
 
-        Champion[] champions = championAPIService.findAllChampion();
+        org.codenova.slseproject.entity.Champion[] champions = championAPIService.findAllChampion();
 
-        List<Champion> championList = Arrays.asList(champions);
+        List<org.codenova.slseproject.entity.Champion> championList = Arrays.asList(champions);
 
         model.addAttribute("champions", championList);
 
